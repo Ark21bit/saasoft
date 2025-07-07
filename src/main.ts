332d88e320119a78ea7aec4cd6a564941cadad46
Ark21bit <webleif@vk.com>
2025-07-08 01:59:44 +0300

    formkit && vueuse install

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import router from './router'
 
 import './assets/main.css'
 import 'virtual:uno.css'
+import { plugin, defaultConfig } from '@formkit/vue'
+import formkitConfig from '../formkit.config.ts'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(plugin, defaultConfig(formkitConfig))
 
 app.mount('#app')
--- uno.config.ts
+++ uno.config.ts
@@ -5,8 +5,20 @@ export default defineConfig({
 	presets: [
 		presetWind4(),
 	],
+
 	transformers: [
 		transformerVariantGroup(),
 		transformerDirectives()
-	]
+	],
+	content: {
+		pipeline: {
+			include: [
+				// the default
+				/\.(vue|svelte|[jt]sx|vine.ts|mdx?|astro|elm|php|phtml|html)($|\?)/,
+				// include js/ts files
+				'src/**/*.{js,ts}',
+				'./formkit.theme.ts'
+			],
+		},
+	},
 })
